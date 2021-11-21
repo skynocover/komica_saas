b36d40fe5598708cb0fa591661628d3d9cef359c
skynocover <skynocover@Gmail.com>
2021-11-21 23:29:01 +0800

    update

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -10,26 +10,23 @@ async function main() {
   //       password: sha256('123456').toString(),
   //     },
   //   });
-
-  await prisma.service.create({
-    data: {
-      name: 'Dota2',
-      topLink: JSON.stringify([
-        { name: 'komica', url: 'https://www.komica.org' },
-        { name: 'Github', url: 'https://github.com/skynocover/komica_nextjs' },
-      ]),
-      headLink: JSON.stringify([
-        { name: 'Dota2', url: 'https://www.dota2.com/home' },
-        { name: 'Dota2 Wiki', url: 'https://dota2.gamepedia.com/Dota_2_Wiki' },
-        { name: 'Underlords', url: 'https://underlords.com' },
-      ]),
-
-      description:
-        '標題及內文為必填,名稱及圖檔為選填  發文限制為每五分鐘三次  回應若不想推文請勾選Sage',
-
-      Owner: { connect: { id: '6191e7115d0be0d33d66f016' } },
-    },
-  });
+  // await prisma.service.create({
+  //   data: {
+  //     name: 'Dota2',
+  //     topLink: JSON.stringify([
+  //       { name: 'komica', url: 'https://www.komica.org' },
+  //       { name: 'Github', url: 'https://github.com/skynocover/komica_nextjs' },
+  //     ]),
+  //     headLink: JSON.stringify([
+  //       { name: 'Dota2', url: 'https://www.dota2.com/home' },
+  //       { name: 'Dota2 Wiki', url: 'https://dota2.gamepedia.com/Dota_2_Wiki' },
+  //       { name: 'Underlords', url: 'https://underlords.com' },
+  //     ]),
+  //     description:
+  //       '標題及內文為必填,名稱及圖檔為選填  發文限制為每五分鐘三次  回應若不想推文請勾選Sage',
+  //     Owner: { connect: { id: '6191e7115d0be0d33d66f016' } },
+  //   },
+  // });
 }
 
 main()
